74ef252ff8725748a240b85deb4654008df772d8
Nur Afifah Binti Nor'Azmi <ifahfifah66@gmail.com>
2024-05-19 23:10:03 +0800

    initial commit

---
+++ lab6_q1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+<title>Lab 6 Q1</title> 
+</head> 
+<body> 
+<?php  
+$name = "Nur Afifah BINTI NOR'AZMI";
+$matric_number = "DI220045";
+$course = "Bachelor of Information technology";
+$year_of_study = "Year 2";
+$address = "No 1, Lorong Permai 2E, Taman Pendamaran Permai 2E, 42000 Port Klang, Selangor";
+?> 
+<table> 
+<tr> 
+<td>Name</td> 
+<td><?php echo "$name"; ?></td>
+
+<td>matric number</td> 
+<td><?php echo "$matric_number"; ?></td>
+
+<td>course</td> 
+<td><?php echo "$course"; ?></td>
+
+<td>year of study</td> 
+<td><?php echo "$year_of_study"; ?></td>
+
+<td>address</td> 
+<td><?php echo "$address"; ?></td>
+
+</tr> 
+</table> 
+</body> 
+</html>
